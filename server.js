1e5ee4d6346f0f96ac304ff385747ff16d45d639
TaeJiramet <674259004@webmail.npru.ac.th>
2026-06-25 16:41:18 +0700

    update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const server = createServer((req, res) => {
         message: "สวัสดีจาก Node.js Server!",
       }),
     );
+  } else if (method === "POST" && url === "/xyz"){
+    res.writeHead(200);
+    res.end(
+        JSON.stringify({
+            message: "สวัสดีจาก POST Method XYZ",
+        }),
+    );
   } else {
     res.writeHead(404);
     res.end(JSON.stringify({ error: "ไมพบหนาที่ตองการ" }));
